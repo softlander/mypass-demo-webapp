5fd33caa8ccf3d9f9c50985850301bebf2ae42ca
Gaurav Tiwari <gaurav1@tutamail.com>
2021-11-12 18:47:36 +0530

    websocket works in dev env

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use('/api', createProxyMiddleware({ target: 'http://localhost:4000/' }));
-    app.use('ws', createProxyMiddleware({ target: 'http://localhost:8000/' }));
+    app.use('/api', createProxyMiddleware({ target: 'http://localhost:4000/', changeOrigin: true }));
+    app.use('/socket.io', createProxyMiddleware({ target: 'http://localhost:8000/', changeOrigin: true, ws: true }));
 }
