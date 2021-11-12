56a3d705c954b797da69b60b35cede59f1343b07
Gaurav Tiwari <gaurav1@tutamail.com>
2021-11-12 09:09:00 +0530

    Fix proxy setup function

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,6 @@
-const proxy = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use(proxy('/api', { target: 'http://localhost:4000/' }))
-    app.use(proxy('/ws', { target: 'http://localhost:8000/' }))
+    app.use('/api', createProxyMiddleware({ target: 'http://localhost:4000/' }));
+    app.use('ws', createProxyMiddleware({ target: 'http://localhost:8000/' }));
 }
