f15c14453e4c1ac1d57ca85c55007e281aab3109
Gaurav Tiwari <gaurav1@tutamail.com>
2021-11-15 18:28:27 +0530

    Make dev env point to prod server for now

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,17 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+/* eslint-disable no-underscore-dangle */
+const __DEV__ = process.env.NODE_ENV !== 'production';
+
+// local dev
+// const API_URL = __DEV__ ? 'http://localhost:4000' : 'https://demo.mypass.id';
+// const WS_URL = __DEV__ ? 'http://localhost:8000' : 'https://demo.mypass.id';
+
+// dev
+const API_URL = __DEV__ ? 'https://demo.mypass.id' : 'https://demo.mypass.id';
+const WS_URL = __DEV__ ? 'https://demo.mypass.id' : 'https://demo.mypass.id';
+
 module.exports = function (app) {
-    app.use('/api', createProxyMiddleware({ target: 'http://localhost:4000/', changeOrigin: true }));
-    app.use('/socket.io', createProxyMiddleware({ target: 'http://localhost:8000/', changeOrigin: true, ws: true }));
+    app.use('/api', createProxyMiddleware({ target: API_URL, changeOrigin: true }));
+    app.use('/socket.io', createProxyMiddleware({ target: WS_URL, changeOrigin: true, ws: true }));
 }
